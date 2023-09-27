f9883b2a33de4221c6a397d41911b88778e44c3e
davidayodele <starcanyonschool@gmail.com>
2023-09-27 15:44:24 -0700

    added notes

--- proj1/src/App.js
+++ proj1/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component {  // child class of component
   render() {
     return (
       <div className="App">
